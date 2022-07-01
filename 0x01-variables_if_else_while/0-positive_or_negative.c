49a9143cbef5d45affc418df68525d042dccebfd
Vince-arch <vincekimani006@gmail.com>
2022-07-01 08:02:08 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,6 +5,11 @@ gcc  -c $CFILE > n
 
 /* betty style doc for function main goes there */
 
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 
 {
