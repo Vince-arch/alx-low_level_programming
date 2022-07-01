3487348e51071a6a3d525fdf443f1243e17e1d60
Vince-arch <vincekimani006@gmail.com>
2022-07-01 13:25:06 -0700

    program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,19 +12,21 @@ int main(void)
 {
 
 	int n;
+	int d:
 
 	srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
+	d = n%10;
 
-	if (n%10 == 0)
-	printf("Last digit of %d", n "is" n%10 "and is 0\n");
+	if (d == 0)
+	printf("Last digit of %d is %d and is 0\n", n, d);
 	
-	else if (n%10 > 5)
-	printf("Last digit of %d", n "is" n%10 "and is greater than 5\n");
+	else if (d >= 6)
+	printf("Last digit of %d  is %d and is greater than 5\n", n, d);
 	
 	else 
-	print("Last digit of %d", n "is" n%10 "and is less than 5\n");
+	print("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
 
 	return (0);
 
