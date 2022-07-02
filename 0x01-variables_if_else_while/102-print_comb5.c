e66555177638d1cc08694a7a90a09a04f7a71b98
Vince-arch <vincekimani006@gmail.com>
2022-07-02 01:02:35 -0700

    program that prints all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -40,19 +40,15 @@ int main(void)
 			putchar((k % 10) + '0');
 
 
-
 			if (i == 98 && k == 99)
 
 				continue;
 
 			putchar(',');
-
 			putchar(' ');															}
 
-		}
+	}
 
 	putchar('\n');
-
 	return (0);
-
 }
